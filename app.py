287f047a7a3a369fcf5fc85507c559d07f112dc9
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-06 17:33:01 -0400

    while loops

--- app.py
+++ app.py
@@ -10,3 +10,39 @@
 #     print('Wrong password')
 
 
+# name = 'Matt'
+# age = 44
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif age < 12:
+#     print('You are not Alice, kiddo')
+# elif age > 2000:
+#     print('Unlike you, Alice is not an undead, immortal vampire.')
+# elif age > 100:
+#     print('You are not Alice, grannie')
+# else:
+#     print('Who are you?')
+  
+
+# print('Enter a name.')
+# name = input()
+# if name != '':
+#       print('Thank you.')
+# else:
+#     print('You did not enter a name.')
+
+# spam = 0
+# while spam < 5:
+#     print('Hello World')
+#     spam = spam + 1
+
+# name = ''
+# while name != 'Matt':
+#   print('Please type YOUR name.')
+#   name = input()
+#   if name == '':
+#     print('You didn\'t enter anything.')
+#   else:
+#     print('Thank you!')
+
+    
