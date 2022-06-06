f4982a0151fff863b6df777f35836a7fce60f4da
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-06-06 17:13:56 -0400

    python condtional flows

---
+++ app.py
@@ -0,0 +1,12 @@
+# name = 'Alice'
+# if name == 'Bob':
+#     print('Hi Alice')
+# print('Done')
+
+# password = 'swordfish'
+# if password == 'swordfish':
+#     print('Access granted')
+# else:
+#     print('Wrong password')
+
+
